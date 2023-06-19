1957dd28321b807c05f14e881dde4ce500e7e449
Suprit Kothari <97793874+M4STERR0SH1@users.noreply.github.com>
2023-06-19 15:18:59 +0530

    Add files via upload

---
+++ Apollo_website/project1_using_pyautogui.py
@@ -0,0 +1,70 @@
+# Import the relevant modules
+import pyautogui
+import time
+
+# Give the python file some time before continuing
+time.sleep(7)
+
+for i in range(999999999999999):
+    # Click on search
+    pyautogui.moveTo(450, 33)
+    time.sleep(1)
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    # Click on saved searches
+    pyautogui.moveTo(277, 543)
+    time.sleep(1)
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    # Click on first option
+    pyautogui.moveTo(146, 842)
+    time.sleep(1)
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    # Click on Net New
+    pyautogui.moveTo(1128, 744)
+    time.sleep(1)
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    # Click on box
+    pyautogui.moveTo(696, 849)
+    time.sleep(1)
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    # Click on select this page
+    pyautogui.moveTo(706, 918)
+    time.sleep(1)
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    # Click on save
+    pyautogui.moveTo(1066, 855)
+    time.sleep(1)
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    # Enter name of list
+    pyautogui.write("try")
+    time.sleep(1)
+
+    # Press enter
+    pyautogui.press("enter")
+    time.sleep(1)
+
+    # Press confirm
+    pyautogui.moveTo(1336, 1475)
+    time.sleep(1)
+    pyautogui.leftClick()
+    time.sleep(1)
+
+    # Press Refresh
+    pyautogui.moveTo(3282, 557)
+    time.sleep(4)
+    pyautogui.leftClick()
+    time.sleep(4)
+
